86368737c6f6f1ebd2b85d2bd353d0b26d0588d2
dhivantro <61950429+dhivantro@users.noreply.github.com>
2022-02-22 03:13:27 +0800

    Add files via upload

--- dhiva_ldpc2/encoder/encoder_test.c
+++ dhiva_ldpc2/encoder/encoder_test.c
@@ -1,10 +1,17 @@
- #include <stdio.h>
- #define row_B 46
+#include <stdio.h>
+#define row_B 46
 #define column_B 68
 #define expFactor 5
+#define column_msg_B 22
+#define sizeC (column_B*expFactor)
 
  int B[row_B][column_B];
  int H[row_B][column_B][expFactor][expFactor];
+ int H_2D [row_B*expFactor] [column_B*expFactor];
+ int codeword[sizeC];
+ int parity[expFactor];
+
+int sizeMsg = expFactor*column_msg_B; //Get from make file ->N
 
  //variables in for loop
  int i=0;
@@ -13,6 +20,292 @@
  int m=0;
  int pK=0;
  int pM=0;
+
+void print_codeword(){
+
+  int i;
+  
+  printf("\n\n--------------------codeword:-------------\n\n");
+  for(i=0; i<sizeC; i++)
+    {
+      printf("%d ", codeword[i]);   
+    }
+
+  printf("\n\n-------------Codeword is printed---------------\n\n");
+}
+
+void append_p1(){
+  //append p1 to codeword
+  for(i=0; i<expFactor; i++)
+    {
+      codeword[i+sizeMsg-1] = parity[i];
+    }
+}
+
+void rotating_p1(int start, int end){
+
+  while (start < end)
+    {
+      int temp = parity[start];
+      parity[start] = parity[end];
+      parity[end] = temp;
+      start++;
+      end--;
+    }
+}
+
+void shifting_p1(int n, int z){
+
+  rotating_p1(0, z-1);
+  rotating_p1(0, n-1);
+  rotating_p1(n, z-1);
+
+  append_p1();
+}
+
+
+void get_parity(int input[]){
+
+  int m, k, i, j, r, z=expFactor;
+  int product[]={}, prodIM[]={};
+
+  //-------------------P1------------------//
+  
+  //reset parity array
+  for(i=0; i<z; i++)
+   {
+     parity[i]=0;
+   }
+
+  //Finding p1
+  for(m=0; m<column_msg_B; m++) //1st x col of H (msg part)
+    {
+      for(k=0; k<4; k++) //1st 4 rows of H
+	{
+	  //printf("\nH Col %d ",m);
+	  // printf("\nH Row %d ",k);
+	  for(i=0; i<z; i++) //First I col... got z bits
+	    {
+	      //printf("\nI col %d:  ",i);
+	      
+	      for(j=0; j<z; j++) //First I row... got z bits
+		{
+		  	       		  
+		   product[j] = input[j + m*z] * H[k][m][j][i];
+		  		  
+		}//j
+
+	      int sum=0;
+	      for(r=0; r<z; r++)
+		{
+		  sum = sum + product[r]; //value of product of every I*m		 
+		}
+	      
+	      prodIM[i] = sum; //will have z size array containing IM elements
+	     		   
+	      
+	    }//i
+	  
+	   //Sum parities at every new row in 1st column (for IM)
+	  //At the end, will have parity array with z bits
+	    for(r=0; r<z; r++)
+	     {
+	       parity[r] = parity[r] + prodIM[r];
+	     }
+	}//k
+    }//m
+
+  //---------------Reshifting for P1--------------//
+  //Now, need to shift the parity[] to get p1[]
+  //Getting value of p1 position in B
+    int n;
+    
+    if (B[1][column_msg_B] == -1)
+      { n = B[2][column_msg_B]; }
+    else
+      { n = B[1][column_msg_B]; }
+
+    shifting_p1( (z-n), z ); //(z-1) so that it shifts to the left
+
+
+    //-------P2 TO P4-------//
+
+    //reset parity array
+      for(i=0; i<z; i++)
+      {
+	parity[i]=0;
+      }
+
+    //traverse by row and Hcolumn in codeword (all msg and one p)
+    
+    for (k=0; k<3; k++) //row H 1 to 3
+      {
+	for (m=0; m<column_msg_B+(k+1); m++) //H column from msg 0 to parity 1,2,3
+	  {
+	    for (i=0; i<z; i++) //I col
+	      {
+		for (j=0; j<z; j++) //I row
+		  {
+		    product[j] = codeword[j + m*z] * H[k][m][j][i];
+		  }//j
+		
+		int sum=0;
+		for(r=0; r<z; r++)
+		  {
+		    sum = sum + product[r]; //value of product of every I*m		 
+		  }
+	      
+		prodIM[i] = sum;
+		
+	      }//i
+	    
+	     //Sum parities at every new row in 1st column (for IM)
+	    for(r=0; r<z; r++)
+	     {
+	       parity[r] = parity[r] + prodIM[r];
+	     }
+	    
+	  }//m
+
+	//append parity array to codeword
+	int w;
+	for(w=0; w<z; w++)
+	  {
+	    codeword[(sizeMsg-1) + (k+1)*w] = parity[w];
+	  }
+
+	//reset parity array before going to new row
+	int y;
+	  for(y=0; y<z; y++)
+	    {
+	      parity[y]=0;
+	      prodIM[y]=0;
+	    }	
+      }//k
+
+    //-------P5 TO PN-------//
+
+    //reset parity array
+      for(i=0; i<z; i++)
+      {
+	parity[i]=0;
+      }
+
+      for (k=4; k<row_B; k++)
+	{
+	  for (m=0; m<(column_msg_B+k); m++)
+	    {
+	      for (i=0; i<z; i++) //I col
+		{
+		  for (j=0; j<z; j++) //I row
+		    {
+
+		      product[j] = codeword[j + m*z] * H[k][m][j][i];
+		      
+		    }//j
+
+		int sum=0;
+		for(r=0; r<z; r++)
+		  {
+		    sum = sum + product[r]; //value of product of every I*m		 
+		  }
+	      
+		prodIM[i] = sum;
+		
+		}//i
+
+	       //Sum parities at every new row in 1st column (for IM)
+	    for(r=0; r<z; r++)
+	     {
+	       parity[r] = parity[r] + prodIM[r];
+	     }
+	    
+	    }//m
+
+	  //append parity array to codeword
+	int w;
+	for(w=0; w<z; w++)
+	  {
+	    codeword[(sizeMsg-1) + k*w] = parity[w];
+	  }
+
+	//reset parity array before going to new row
+	int y;
+	  for(y=0; y<z; y++)
+	    {
+	      parity[y]=0;
+	      prodIM[y]=0;
+	    }
+	  
+	}//k
+      
+}//get_parity fx
+
+
+void codeword(){
+
+  int i, j, k;
+
+  for(i=0; i<sizeC; i++)
+    {
+      codeword[i] = 0;
+    }
+
+   int sizeMsg = expFactor*column_msg_B;
+
+   int input [] = {0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1};
+
+
+    //Append message or input bits to codeword array
+  for (k=0; k<sizeMsg; k++) //row index of input
+    {
+      if (k>sizeMsg && k<=sizeC)
+	{codeword[k] = 0;}
+      
+      else
+	{ codeword[k] = input[k];}  
+    }
+
+  get_parity(input);
+}
+
+void convert(){
+
+  int k, m, i ,j;
+
+   printf("\n\n-----------------------------------------\n\n");
+   printf("Converting 4D H to 2D H");
+   printf("\n\n-----------------------------------------\n\n");
+   
+   for (k=0; k<row_B; k++)  //row B
+    {
+      for (i=0; i<expFactor; i++)
+	{
+	  for (m=0; m<column_B; m++) //column B
+	    {
+	      for (j=0; j<expFactor; j++)
+		{
+		  H_2D[((expFactor-1)*k)+k+i][((expFactor-1)*m)+m+j] = H[k][m][i][j];
+		  //printf("\n k:%d m:%d i:%d j:%d index:[%d][%d]   H_old: %d Hnew: %d",k,m,i,j, ((5-1)*k)+k+i, ((5-1)*m)+m+j,  H[k][m][i][j], Hnew[(3*k)+i][(3*m)+j]);
+		}
+	    }
+	}
+    }
+  printf("\n\n");
+
+  for (i=0; i<(row_B*expFactor); i++)
+    {
+      for (j=0; j<(column_B*expFactor); j++)
+	{
+	  printf("%d",H_2D[i][j]);
+	}
+      printf("\n");
+    }
+
+  printf("\n\n\n Finish converting to 2D H  \n\n\n");
+
+}
+
 
  void zeros_matrix()
  {
@@ -139,5 +432,8 @@ void positive_matrix()
  	    }
  	}
      }
-   print_All();
+   //print_All();
+   codeword();
+   // convert(); //Convert 4D H to 2D h
+   print_codeword();
  }
